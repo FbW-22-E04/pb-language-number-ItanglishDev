77b28805584eb59fb2054ebb7e5d8c770e4179d1
chris <chrishethertonweb@gmail.com>
2022-07-05 13:11:53 +0200

    second exercise executed

--- solution.js
+++ solution.js
@@ -4,3 +4,9 @@ let age = 25;
 console.log(introSentence + " " + age);
 
 // The operator allow to concatenate
+
+let integer = "1005";
+console.log(parseInt(integer));
+
+let float = "10.05";
+console.log(parseFloat(float));
